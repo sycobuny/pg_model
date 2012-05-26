66b55ddfd62bf9d7ff4c869e78b9b423651277e3
Stephen Belcher <sbelcher@gmail.com>
2012-05-26 19:53:38 -0400

    Add _QueryLiteral API.
    * new _QueryLiteral(string)
      Takes a string, _QueryExpression, or object with __toString()
      defined, and returns a _QueryLiteral object based on the contents.
    * _QueryLiteral::create(string)
      Performs the same as new _QueryLiteral(), but, if a _QueryLiteral is
      the object passed in, it simply returns that object rather than
      creating a new one.
    * Query::lit(string)
      A syntactical sugar wrapper around _QueryLiteral::create().
    
    Issues:
    * Addresses #14 on GitHub.

--- sql_generator/literal_expression.php
+++ sql_generator/literal_expression.php
@@ -1,6 +1,39 @@
 <?php
 
     class _QueryLiteral implements _QueryExpression {
+        private $string;
+
+        function __construct($string) {
+            if (is_a($string, '_QueryExpression')) {
+                $string = $string->sql_string();
+            }
+            else if (is_object($string)) {
+                if (method_exists($string, '__toString')) {
+                    $string = "$string";
+                }
+                else {
+                    /* TODO: blow up */
+                }
+            }
+            else if (is_array($string)) {
+                /* TODO: blow up */
+            }
+
+            $this->string = "$string";
+        }
+
+        public static function create($string) {
+            if (is_a($string, '_QueryLiteral')) {
+                return $string;
+            }
+            else {
+                return new _QueryLiteral($string);
+            }
+        }
+
+        public function sql_string() {
+            return $this->string;
+        }
     }
 
 ?>
--- sql_generator/query.php
+++ sql_generator/query.php
@@ -1,6 +1,9 @@
 <?php
 
     class Query extends _QueryTableExpression {
+        public static function lit($string) {
+            return _QueryLiteral::create($string);
+        }
     }
 
 ?>
