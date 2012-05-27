41bda81a2ccc6994159d4e22881f09d6cfab3d98
Stephen Belcher <sbelcher@gmail.com>
2012-05-26 20:33:21 -0400

    Add _QueryString API.
    * new _QueryString(string)
      Takes a string, _QueryExpression, or object with __toString()
      defined, and returns a _QueryString object based on the contents.
    * _QueryString::create(string)
      Performs the same as new _QueryString(), but, if a _QueryString is
      the object passed in, it simply returns that object rather than
      creating a new one.
    * Query::string(string)
      A syntactical sugar wrapper around _QueryString::create().
    
    Issues:
    * Addresses #14 on GitHub.

--- sql_generator/query.php
+++ sql_generator/query.php
@@ -13,6 +13,19 @@
         public static function lit($string) {
             return _QueryLiteral::create($string);
         }
+
+        /**
+         * _QueryString sugary constructor
+         *
+         * Constructs a _QueryString object, or simply returns a
+         * _QueryString object if passed a preconstructed one.
+         *
+         * @param string $string The SQL string to include
+         * @return _QueryString
+         */
+        public static function string($string) {
+            return _QueryString::create($string);
+        }
     }
 
 ?>
--- sql_generator/string.php
+++ sql_generator/string.php
@@ -1,6 +1,59 @@
 <?php
 
     class _QueryString extends _QueryValueExpression {
+        private $string;
+
+        /**
+         * _QueryString constructor
+         *
+         * @param string $string The SQL string to include
+         * @return _QueryString
+         */
+        function __construct($string) {
+            if (is_a($string, '_QueryString')) {
+                $string = $string->string;
+            }
+            else if (is_a($string, '_QueryExpression')) {
+                $string = $string->sql_string();
+            }
+            else if ((is_object($string) &&
+                      !method_exists($string, '__toString')) ||
+                     is_array($string)) {
+                /* TODO: blow up */
+            }
+
+            $this->string = "$string";
+        }
+
+        /**
+         * _QueryString sugary constructor
+         *
+         * Constructs a _QueryString object, or simply returns a
+         * _QueryString object if passed a preconstructed one.
+         *
+         * @param string $string The SQL string to include
+         * @return _QueryLiteral
+         */
+        public static function create($string) {
+            if (is_a($string, '_QueryString')) {
+                return $string;
+            }
+            else {
+                return new _QueryString($string);
+            }
+        }
+
+        /**
+         * Produce SQL code from this object.
+         *
+         * Processes this expression and returns the resulting SQL
+         * string.
+         *
+         * @return string
+         */
+        public function sql_string() {
+            return "'" . pg_escape_string($this->string) . "'";
+        }
     }
 
 ?>
