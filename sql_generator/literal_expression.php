4d693cfeb8d9ff41874d357df8fb7b565f1d8d90
Stephen Belcher <sbelcher@gmail.com>
2012-05-26 20:04:19 -0400

    Document _QueryLiteral API.

--- sql_generator/literal_expression.php
+++ sql_generator/literal_expression.php
@@ -3,6 +3,12 @@
     class _QueryLiteral implements _QueryExpression {
         private $string;
 
+        /**
+         * _QueryLiteral constructor
+         *
+         * @param string $string The literal SQL to include
+         * @return _QueryLiteral
+         */
         function __construct($string) {
             if (is_a($string, '_QueryExpression')) {
                 $string = $string->sql_string();
@@ -22,6 +28,15 @@
             $this->string = "$string";
         }
 
+        /**
+         * _QueryLiteral sguary constructor
+         *
+         * Constructs a _QueryLiteral object (or simply returns an
+         * the _QueryLiteral object if passed a preconstructed one.
+         *
+         * @param string $string The literal SQL to include
+         * @return _QueryLiteral
+         */
         public static function create($string) {
             if (is_a($string, '_QueryLiteral')) {
                 return $string;
@@ -31,6 +46,14 @@
             }
         }
 
+        /**
+         * Produce SQL code from this object.
+         *
+         * Processes this expression and returns the resulting SQL
+         * string.
+         *
+         * @return string
+         */
         public function sql_string() {
             return $this->string;
         }
--- sql_generator/query.php
+++ sql_generator/query.php
@@ -1,6 +1,15 @@
 <?php
 
     class Query extends _QueryTableExpression {
+        /**
+         * _QueryLiteral sugary constructor
+         *
+         * Constructs a _QueryLiteral object (or simply returns an
+         * the _QueryLiteral object if passed a preconstructed one.
+         *
+         * @param string $string The literal SQL to include
+         * @return _QueryLiteral
+         */
         public static function lit($string) {
             return _QueryLiteral::create($string);
         }
